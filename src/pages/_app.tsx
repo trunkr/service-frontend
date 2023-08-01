48cc8449221b96edd1bd6109bf486e3cc21afb10
JjungminLee <julie0964@naver.com>
2023-08-02 06:41:55 +0900

    feat:project setting

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RecoilRoot } from 'recoil';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  // 이렇게 해야 서로 다른 사용자와 요청 사이에 데이터가 공유되지 않는다.
+  const [queryClient] = React.useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
+    </QueryClientProvider>
+  );
 }
+
+export default MyApp;
