131e15171e9d6ef77fc23bd75d1f48491415aa72
JjungminLee <julie0964@naver.com>
2023-09-20 15:25:23 +0900

    feat:add font,logo,favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,18 @@ import type { AppProps } from 'next/app';
 import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RecoilRoot } from 'recoil';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  // 이렇게 해야 서로 다른 사용자와 요청 사이에 데이터가 공유되지 않는다.
   const [queryClient] = React.useState(() => new QueryClient());
 
   return (
     <QueryClientProvider client={queryClient}>
       <RecoilRoot>
+        <Head>
+          <title>Trunkr</title>
+          <link rel="shortcut icon" href="./assets/logo/favicon_large.svg" />
+        </Head>
         <Component {...pageProps} />
       </RecoilRoot>
     </QueryClientProvider>
