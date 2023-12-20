ceefd6b12644565e213ee50c3af2bbb91e4ea79f
JjungminLee <julie0964@naver.com>
2023-12-20 11:54:03 +0900

    refactor:kakao login js sdk설정

--- src/components/commons/buttons/login/KakaoLoginButton.tsx
+++ src/components/commons/buttons/login/KakaoLoginButton.tsx
@@ -1,14 +1,19 @@
 import KakaoLogin from '@public/loginBtn/Kakao_Button.svg';
-import { useRouter } from 'next/router';
+//import { useRouter } from 'next/router';
 
 const KakaoLoginButton = () => {
-  const router = useRouter();
-  const REST_API_KEY = process.env.NEXT_PUBLIC_KAKAO_REST_APT_KEY;
+  //const router = useRouter();
+  //const REST_API_KEY = process.env.NEXT_PUBLIC_KAKAO_REST_APT_KEY;
   const REDIRECT_URI = process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI;
-  const LINK = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+  //const LINK = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
   function kakaoLogin() {
-    router.push(LINK);
+    // router.push(LINK);
+    console.log(REDIRECT_URI);
+    window.Kakao.Auth.authorize({
+      redirectUri: REDIRECT_URI,
+      scope: 'account_email,openid',
+    });
   }
   return (
     <div onClick={kakaoLogin}>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,24 @@ import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { RecoilRoot } from 'recoil';
 import Head from 'next/head';
+import Script from 'next/script';
+
+declare global {
+  interface Window {
+    Kakao: any;
+    kakao: any;
+  }
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = React.useState(() => new QueryClient());
+
+  function kakaoInit() {
+    // 페이지가 로드되면 실행
+    if (!window.Kakao.isInitialized()) {
+      window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_JS_KEY);
+    }
+  }
 
   return (
     <QueryClientProvider client={queryClient}>
@@ -15,6 +30,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           <link rel="shortcut icon" href="./assets/logo/favicon_large.svg" />
         </Head>
         <Component {...pageProps} />
+        <Script src="https://developers.kakao.com/sdk/js/kakao.js" onLoad={kakaoInit}></Script>
       </RecoilRoot>
     </QueryClientProvider>
   );
